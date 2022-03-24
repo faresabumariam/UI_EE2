8f7baf486d6d3a4070e7717a23e65e007dc7f6eb
SenneDG <senne.degreef@student.kuleuven.be>
2022-03-24 15:14:56 +0100

    Sliders settingsFrame

--- src/settingsFrame.java
+++ src/settingsFrame.java
@@ -1,8 +1,17 @@
 import javax.swing.*;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 public class settingsFrame extends JFrame {
     private JButton backButton;
@@ -12,7 +21,8 @@ public class settingsFrame extends JFrame {
     private JLabel sound;
     private JSlider slider1;
     private JSlider slider2;
-
+    private int value1;
+    private int value2;
 
     public settingsFrame(String title)
     {
@@ -28,7 +38,52 @@ public class settingsFrame extends JFrame {
             };
         });
 
+        slider2.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                value1 = slider2.getValue();
+                makeGETRequest("https://studev.groept.be/api/a21ib2d04/lightsButton_input/"+ String.valueOf(value1));
+            }
+        });
 
+        slider1.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                value2 = slider1.getValue();
+                makeGETRequest("https://studev.groept.be/api/a21ib2d04/soundButton_input/"+ String.valueOf(value2));
+            }
+        });
+
+    }
+
+    public String makeGETRequest(String urlName){
+        BufferedReader rd = null;
+        StringBuilder sb = null;
+        String line = null;
+        try {
+            URL url = new URL(urlName);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            sb = new StringBuilder();
+            while ((line = rd.readLine()) != null)
+            {
+                sb.append(line + '\n');
+            }
+            conn.disconnect();
+            return sb.toString();
+        }
+        catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+        catch (ProtocolException e){
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        return "";
 
     }
 
@@ -36,3 +91,8 @@ public class settingsFrame extends JFrame {
 
 
 }
+
+
+
+
+
